7aa5084fdd3826a83a4d31301c95ddfda18a7cea
Lisa Solovyeva <lisa.solovyeva@schibsted.com>
2019-12-29 17:58:05 +0100

    assignment 1

---
+++ hw01-maven/src/main/java/HelloOtus.java
@@ -0,0 +1,15 @@
+import com.google.common.base.Optional;
+
+public class HelloOtus {
+    public static void main(String args[]) {
+        HelloOtus guavaTester = new HelloOtus();
+
+        Optional<Integer> a =  Optional.of(5);
+        Optional<Integer> b =  Optional.of(10);
+        System.out.println(guavaTester.sum(a,b));
+    }
+
+    public Integer sum(Optional<Integer> a, Optional<Integer> b) {
+        return a.get() + b.get();
+    }
+}
